2262ae36d41d8c18a0ed4fa32c8e16cceba33d8e
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2020-12-30 20:48:50 +0100

    some work for more reivew info

--- tests/privacy_webbkollen.py
+++ tests/privacy_webbkollen.py
@@ -31,58 +31,107 @@ def run_test(langCode, url):
 
     print(_('TEXT_RUNNING_TEST'))
 
-    api_lang_code = 'en'
-    if langCode == 'sv':
-        api_lang_code = 'sv'
-    elif langCode == 'de':
-        api_lang_code = 'de'
-    elif langCode == 'no':
-        api_lang_code = 'no'
-
     url = 'https://webbkoll.dataskydd.net/{1}/check?url={0}'.format(
-        url.replace('/', '%2F').replace(':', '%3A'), api_lang_code)
+        url.replace('/', '%2F').replace(':', '%3A'), langCode)
     headers = {
         'user-agent': 'Mozilla/5.0 (compatible; Webperf; +https://webperf.se)'}
-    request = requests.get(url, allow_redirects=False,
-                           headers=headers, timeout=request_timeout*2)
+    request = requests.get(url, allow_redirects=True,
+                           headers=headers, timeout=request_timeout)
 
     time.sleep(20)
 
     # hämta det faktiska resultatet
-    soup = BeautifulSoup(request.text, 'html.parser')
-    final_url = None
-    for link in soup.find_all('a'):
-        final_url = 'https://webbkoll.dataskydd.net{0}'.format(
-            link.get('href'))
+    soup2 = BeautifulSoup(request.text, 'html.parser')
+    # final_url = None
+    # for link in soup.find_all('a'):
+    #    final_url = 'https://webbkoll.dataskydd.net{0}'.format(
+    #        link.get('href'))
 
-    if final_url != None:
-        request2 = requests.get(
-            final_url, allow_redirects=True, headers=headers, timeout=request_timeout*2)
-        soup2 = BeautifulSoup(request2.text, 'html.parser')
+    if True:  # final_url != None:
+        # request2 = requests.get(
+        #    final_url, allow_redirects=True, headers=headers, timeout=request_timeout*2)
+        # soup2 = BeautifulSoup(request2.text, 'html.parser')
         summary = soup2.find_all("div", class_="summary")
         results = soup2.find_all(class_="result")
+        result_title = soup2.find(id="results-title")
+        if not result_title:
+            print(
+                'Error! Unfortunately the request for URL "{0}" failed, message:\nUnexpected result')
+            return (-1.0, '* TEST FAILED', return_dict)
+
+        review_messages = ''
 
         for result in results:
             header = result.find("h3")
+            header_id = header.get('id')
+            if header_id == 'what' or header_id == 'raw-headers':
+                continue
             # print(type(header.contents))
-            #- alert
-            points -= (len(header.find_all("i", class_="alert")) * 1.0)
-            #- warning
-            points -= (len(header.find_all("i", class_="warning")) * 0.5)
+            number_of_success = len(header.find_all("i", class_="success"))
 
+            # - alert
+            number_of_alerts = len(header.find_all("i", class_="alert"))
+            points -= (number_of_alerts * 1.0)
+
+            # - warning
+            number_of_warnings = len(header.find_all("i", class_="warning"))
+            points -= (number_of_warnings * 1.0)
+
+            number_of_sub_alerts = 0
+            number_of_sub_warnings = 0
             divs = result.find_all("div")
-            for div in divs:
+
+            more_info = ''
+            if len(divs) > 0:
+                div = divs[0]
                 # print(type(h3a.contents))
-                #-- alert
-                points -= (len(div.find_all("i", class_="alert")) * 0.1)
-                #-- warning
-                points -= (len(div.find_all("i", class_="warning")) * 0.05)
+                # -- alert
+                number_of_sub_alerts = len(div.find_all("i", class_="alert"))
+                points -= (number_of_sub_alerts * 0.1)
+                # -- warning
+                number_of_sub_warnings = len(
+                    div.find_all("i", class_="warning"))
+                points -= (number_of_sub_warnings * 0.05)
 
-        mess = ''
+            paragraphs = result.find_all("p")
+            if len(paragraphs) > 0:
+                for paragraph_text in paragraphs[0].strings:
+                    more_info += paragraph_text + " "
+            else:
+                more_info = "!" + result.text
+            more_info = more_info.replace("  ", " ").strip()
 
-        for line in summary:
-            mess += '* {0}'.format(re.sub(' +', ' ', line.text.strip()).replace(
-                '\n', ' ').replace('    ', '\n* ').replace('Kolla upp', '').replace('Look up', '').replace('  ', ' '))
+            review_messages += '* ' + header.text.strip()
+            if number_of_success > 0:
+                review_messages += ": VERY_GOOD.\n"
+            elif number_of_alerts > 0:
+                review_messages += ": VERY_BAD.\n"
+            elif number_of_warnings > 0:
+                review_messages += ": BAD.\n"
+            elif number_of_sub_alerts > 0 and number_of_sub_warnings > 0:
+                review_messages += ": BAD, with {0} error(s) and {1} warning(s).\n".format(
+                    number_of_sub_alerts, number_of_sub_warnings)
+            elif number_of_sub_alerts > 0:
+                review_messages += ": OK, but with {0} error(s).\n".format(
+                    number_of_sub_alerts)
+            elif number_of_sub_warnings > 0:
+                review_messages += ": GOOD, but with {0} warning(s).".format(
+                    number_of_sub_warnings)
+            else:
+                review_messages += ": " + more_info + "\n"
+
+        # give us result date (for when dataskydd.net generated report)
+        result_title_beta = result_title.find_all('div', class_="beta")
+        if len(result_title_beta) > 0:
+            for header_info in result_title_beta[0].strings:
+                info = header_info.strip()
+                if info.startswith('20'):
+                    review_messages += "* GENERATED: " + info + ".\n"
+
+        #review_messages += "\nAdditional info:\n"
+        # for line in summary:
+        #    review_messages += '* {0}'.format(re.sub(' +', ' ', line.text.strip()).replace(
+        #        '\n', ' ').replace('    ', '\n* ').replace('Kolla upp', '').replace('Look up', '').replace('  ', ' '))
 
         if points == 5:
             review = ('TEXT_REVIEW_VERY_GOOD')
@@ -96,6 +145,6 @@ def run_test(langCode, url):
             review = _('TEXT_REVIEW_IS_VERY_BAD')
             points = 1.0
 
-        review += mess
+        review += review_messages
 
         return (points, review, return_dict)

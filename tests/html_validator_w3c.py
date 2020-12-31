f60808ada34e8f25f5af414e4f9ab4646cae38f0
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2020-12-31 22:40:28 +0100

    new rating system for html

--- tests/html_validator_w3c.py
+++ tests/html_validator_w3c.py
@@ -54,22 +54,6 @@ def run_test(langCode, url):
         print('Timeout!\nMessage:\n{0}'.format(sys.exc_info()[0]))
         return None
 
-    if number_of_errors == 0:
-        points = 5.0
-        review = _('TEXT_REVIEW_HTML_VERY_GOOD')
-    elif number_of_errors <= 5:
-        points = 4.0
-        review = _('TEXT_REVIEW_HTML_IS_GOOD').format(number_of_errors)
-    elif number_of_errors <= 15:
-        points = 3.0
-        review = _('TEXT_REVIEW_HTML_IS_OK').format(number_of_errors)
-    elif number_of_errors <= 30:
-        points = 2.0
-        review = _('TEXT_REVIEW_HTML_IS_BAD').format(number_of_errors)
-    elif number_of_errors > 30:
-        points = 1.0
-        review = _('TEXT_REVIEW_HTML_IS_VERY_BAD').format(number_of_errors)
-
     error_message_dict = {}
     error_message_grouped_dict = {}
     if number_of_errors > 0:
@@ -97,4 +81,34 @@ def run_test(langCode, url):
 
                 review += _('TEXT_REVIEW_ERRORS_ITEM').format(item_text, item_value)
 
+    number_of_error_types = len(error_message_grouped_dict)
+
+    points = calculate_rating(number_of_error_types, number_of_errors)
+
+    if points == 5.0:
+        review = _('TEXT_REVIEW_HTML_VERY_GOOD')
+    elif points >= 4.0:
+        review = _('TEXT_REVIEW_HTML_IS_GOOD').format(number_of_errors)
+    elif points >= 3.0:
+        review = _('TEXT_REVIEW_HTML_IS_OK').format(number_of_errors)
+    elif points > 1.0:
+        review = _('TEXT_REVIEW_HTML_IS_BAD').format(number_of_errors)
+    elif points <= 1.0:
+        review = _('TEXT_REVIEW_HTML_IS_VERY_BAD').format(number_of_errors)
+
     return (points, review, error_message_dict)
+
+
+def calculate_rating(number_of_error_types, number_of_errors):
+    rating = 0.0
+
+    rating = 5.0 - (number_of_error_types / 5.0)
+
+    rating2 = ((number_of_errors / 2.0) / 5.0)
+
+    rating3 = rating - rating2
+    rating_result = rating3
+    if rating3 < 1.0:
+        rating_result = 1.0
+
+    return rating_result

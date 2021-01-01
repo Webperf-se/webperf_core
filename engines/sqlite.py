a2f26b8795aa2c244b7bf6f95741344293aa68f4
Johan Kronberg <johan.kronberg@netrelations.com>
2021-01-01 17:48:58 +0100

    Changed sitetests-INSERT in sqlite.py to escape characters better - info from https://stackoverflow.com/questions/3952543/python-and-sqlite-escape-input

--- engines/sqlite.py
+++ engines/sqlite.py
@@ -78,12 +78,7 @@ def write_tests(output_filename, siteTests):
         conn.commit()
 
         # update testresult for all sites
-        format_str = """INSERT INTO sitetests (site_id, test_date, type_of_test, check_report, json_check_data, most_recent, rating)
-        VALUES ("{siteid}", "{testdate}", "{testtype}", "{report}", "{json}", "{recent}", "{rating}");\n"""
-        sql_command = format_str.format(siteid=test["site_id"], testdate=test["date"], testtype=test["type_of_test"],
-                                        report=test["report"], json=test["data"], recent=1, rating=test["rating"])
-
-        c.execute(sql_command)
+        c.execute('INSERT INTO sitetests (site_id, test_date, type_of_test, check_report, json_check_data, most_recent, rating) VALUES (?, ?, ?, ?, ?, ?, ?);\n', (test["site_id"], test["date"], test["type_of_test"], test["report"], test["data"], 1, test["rating"]))
         conn.commit()
 
     conn.close()

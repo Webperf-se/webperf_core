3e5c58e4e0815a536ecb42cc6efd1c5be0d84062
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2021-01-11 19:46:11 +0100

    adjusted how points are calculated

--- tests/performance_sitespeed_io.py
+++ tests/performance_sitespeed_io.py
@@ -1,10 +1,10 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import sys
 import socket
 import ssl
 import json
 import requests
-import urllib # https://docs.python.org/3/library/urllib.parse.html
+import urllib  # https://docs.python.org/3/library/urllib.parse.html
 import uuid
 import re
 import docker
@@ -14,73 +14,87 @@ from tests.utils import *
 import gettext
 _ = gettext.gettext
 
+
 def run_test(langCode, url):
-	"""
-	Checking an URL against Sitespeed.io (Docker version). 
-	For installation, check out:
-	- https://hub.docker.com/r/sitespeedio/sitespeed.io/
-	- https://www.sitespeed.io
-	"""
-	arg = '--rm --shm-size=1g -b chrome --plugins.remove screenshot --browsertime.videoParams.createFilmstrip false --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(config.sitespeed_iterations, url) 
+    """
+    Checking an URL against Sitespeed.io (Docker version). 
+    For installation, check out:
+    - https://hub.docker.com/r/sitespeedio/sitespeed.io/
+    - https://www.sitespeed.io
+    """
+    arg = '--rm --shm-size=1g -b chrome --plugins.remove screenshot --browsertime.videoParams.createFilmstrip false --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(
+        config.sitespeed_iterations, url)
 
-	image = "sitespeedio/sitespeed.io:latest"
+    image = "sitespeedio/sitespeed.io:latest"
 
-	language = gettext.translation('performance_sitespeed_io', localedir='locales', languages=[langCode])
-	language.install()
-	_ = language.gettext
+    language = gettext.translation(
+        'performance_sitespeed_io', localedir='locales', languages=[langCode])
+    language.install()
+    _ = language.gettext
 
-	print(_('TEXT_RUNNING_TEST'))
+    print(_('TEXT_RUNNING_TEST'))
 
-	docker_client = docker.from_env()
-	result = str(docker_client.containers.run(image, arg))
-	result = result.replace('\\n', ' ')
+    docker_client = docker.from_env()
+    result = str(docker_client.containers.run(image, arg))
+    result = result.replace('\\n', ' ')
 
-	old_val = None
-	old_val_unsliced = None
-	result_dict = {}
+    old_val = None
+    old_val_unsliced = None
+    result_dict = {}
 
-	for line in result.split(' '):
-		if old_val == 'speedindex' or old_val == 'load' or old_val == 'backendtime' or old_val == 'firstpaint' or old_val == 'firstvisualchange' or old_val == 'domcontentloaded' or old_val == 'visualcomplete85' or old_val == 'lastvisualchange' or old_val == 'rumspeedindex' or old_val == 'dominteractivetime' or old_val == 'domcontentloadedtime' or old_val == 'pageloadtime' or old_val == 'perceptualspeedindex':
-			result_dict[old_val] = line.replace('ms', '')
-		
-		if line[:-1].lower() == 'requests':
-			result_dict['requests'] = old_val_unsliced
+    for line in result.split(' '):
+        if old_val == 'speedindex' or old_val == 'load' or old_val == 'backendtime' or old_val == 'firstpaint' or old_val == 'firstvisualchange' or old_val == 'domcontentloaded' or old_val == 'visualcomplete85' or old_val == 'lastvisualchange' or old_val == 'rumspeedindex' or old_val == 'dominteractivetime' or old_val == 'domcontentloadedtime' or old_val == 'pageloadtime' or old_val == 'perceptualspeedindex':
+            result_dict[old_val] = line.replace('ms', '')
 
-		old_val = line[:-1].lower()
-		old_val_unsliced = line
-	
-	if 's' in result_dict['speedindex']:
-		"""
-		Changes speedindex to a number if for instance 1.1s it becomes 1100
-		"""
-		result_dict['speedindex'] = int(float(result_dict['speedindex'].replace('s', '')) * 1000)
-	
-	speedindex = int(result_dict['speedindex'])
+        if line[:-1].lower() == 'requests':
+            result_dict['requests'] = old_val_unsliced
 
-	review = ''
+        old_val = line[:-1].lower()
+        old_val_unsliced = line
 
-	if speedindex <= 500:
-		points = 5
-		review = _("TEXT_REVIEW_VERY_GOOD")
-	elif speedindex <= 1200:
-		points = 4
-		review = _("TEXT_REVIEW_IS_GOOD")
-	elif speedindex <= 2500:
-		points = 3
-		review = _("TEXT_REVIEW_IS_OK")
-	elif speedindex <= 3999:
-		points = 2
-		review = _("TEXT_REVIEW_IS_BAD")
-	elif speedindex > 3999:
-		points = 1
-		review = _("TEXT_REVIEW_IS_VERY_BAD")
+    if 's' in result_dict['speedindex']:
+        """
+        Changes speedindex to a number if for instance 1.1s it becomes 1100
+        """
+        result_dict['speedindex'] = int(
+            float(result_dict['speedindex'].replace('s', '')) * 1000)
 
-	review += '* Speedindex: {}\n'.format(speedindex)
-	if 's' in result_dict['load']:
-		review += _("TEXT_REVIEW_LOAD_TIME").format(result_dict['load'])
-	else:
-		review += _("TEXT_REVIEW_LOAD_TIME_SECONDS").format(result_dict['load'])
-	
-	review += _("TEXT_REVIEW_NUMBER_OF_REQUESTS").format(result_dict['requests'])
+    speedindex = int(result_dict['speedindex'])
 
-	return (points, review, result_dict)
+    review = ''
+
+    # give 0.5 seconds in credit
+    speedindex_adjusted = speedindex - 500
+    if speedindex_adjusted <= 0:
+        # speed index is 500 or below, give highest score
+        points = 5.0
+    else:
+        points = 5.0 - (speedindex_adjusted / 1000)
+
+        if points > 5.0:
+            points = 5.0
+        if points < 1.0:
+            points = 1.0
+
+    if points == 5.0:
+        review = _('TEXT_REVIEW_VERY_GOOD')
+    elif points >= 4.0:
+        review = _('TEXT_REVIEW_IS_GOOD')
+    elif points >= 3.0:
+        review = _('TEXT_REVIEW_IS_OK')
+    elif points > 1.0:
+        review = _('TEXT_REVIEW_IS_BAD')
+    elif points <= 1.0:
+        review = _('TEXT_REVIEW_IS_VERY_BAD')
+
+    review += '* Speedindex: {}\n'.format(speedindex)
+    if 's' in result_dict['load']:
+        review += _("TEXT_REVIEW_LOAD_TIME").format(result_dict['load'])
+    else:
+        review += _("TEXT_REVIEW_LOAD_TIME_SECONDS").format(
+            result_dict['load'])
+
+    review += _("TEXT_REVIEW_NUMBER_OF_REQUESTS").format(
+        result_dict['requests'])
+
+    return (points, review, result_dict)

547e10cf9a2c6ecb517fb424649bc9a2c73fda15
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2021-01-19 20:26:25 +0100

    adjusting how rating is done, we now calculate rating on ALL CSS

--- tests/css_validator_w3c.py
+++ tests/css_validator_w3c.py
@@ -22,6 +22,7 @@ css_review_group_errors = config.css_review_group_errors
 css_scoring_method = config.css_scoring_method
 
 global css_features
+global css_properties_doesnt_exist
 
 
 def run_test(langCode, url):
@@ -39,65 +40,28 @@ def run_test(langCode, url):
 
     print(_('TEXT_RUNNING_TEST'))
 
-    results = list()
-
-    # 0. Get list of CSS property names from MDN Web Docs
-    #css_features = get_mdn_web_docs_css_features(True)
+    errors = list()
 
     # 1. Get ROOT PAGE HTML
     html = get_source(url)
     # 2. FIND ALL INLE CSS (AND CALCULTE)
     # 2.1 FINS ALL <STYLE>
-    results_style_tags = get_errors_for_style_tags(html, css_features, _)
-    for result in results_style_tags:
-        results.append(result)
+    errors += get_errors_for_style_tags(html, _)
 
     # 2.2 FIND ALL style=""
-    results_style_attributes = get_errors_for_style_attributes(
-        html, css_features, _)
-    for result in results_style_attributes:
-        results.append(result)
+    errors += get_errors_for_style_attributes(html, _)
 
     # 2.3 GET ERRORS FROM SERVICE
     # 2.4 CALCULATE SCORE
     # 3 FIND ALL <LINK> (rel=\"stylesheet\")
-    results_link_tags = get_errors_for_link_tags(html, css_features, url, _)
-    for result in results_link_tags:
-        results.append(result)
+    errors += get_errors_for_link_tags(html, url, _)
 
-    # 4 COMBINE SCORE(s)
-    # Medelvärdsuträkning för alla resultat
-    if css_scoring_method == 'average':
-        number_of_results = len(results)
-        points = 0.0
-        error_message_dict = {}
-        for result in results:
-            current_points = result[0]
-            points += current_points
-            if current_points < 5.0:
-                review += result[1]
-    elif css_scoring_method == 'median':
-        # Medelvärde för de resultat som EJ fått 5.0 av 5.0
-        number_of_results = 0
-        points = 0.0
-        error_message_dict = {}
-        for result in results:
-            current_points = result[0]
-            if current_points < 5.0:
-                review += result[1]
-                number_of_results += 1
-                points += current_points
-    else:
-        # Använd lägsta värdet för alla resultat som värde
-        number_of_results = 1
-        points = 5.0
-        error_message_dict = {}
-        for result in results:
-            current_points = result[0]
-            if current_points < points:
-                review += result[1]
-                number_of_results = 1
-                points = current_points
+    result = create_review_and_rating(errors, _, '')
+
+    number_of_results = 1
+    points = result[0]
+    review += result[1]
+    error_message_dict = result[2]
 
     points = float("{0:.3f}".format(points / number_of_results))
 
@@ -121,21 +85,25 @@ def run_test(langCode, url):
     return (points, review, error_message_dict)
 
 
-def get_errors_for_link_tags(html, css_features, url, _):
+def get_errors_for_link_tags(html, url, _):
+    #print('link tag(s)')
     results = list()
 
-    regex = r"(?P<markup><link[^>]+(href|src)=[\"|'](?P<resource>[^\"|']+)[^>]*>)"
-    matches = re.finditer(regex, html, re.MULTILINE)
+    soup = BeautifulSoup(html, 'lxml')
+    elements = soup.find_all('link')
 
     o = urllib.parse.urlparse(url)
     parsed_url = '{0}://{1}'.format(o.scheme, o.netloc)
     parsed_url_scheme = o.scheme
 
     resource_index = 1
-    for matchNum, match in enumerate(matches, start=1):
-        markup = match.group('markup')
-        if 'stylesheet' in markup:
-            resource_url = match.group('resource')
+    for element in elements:
+        # print(element.contents)
+        resource_type = element['rel']
+        if 'stylesheet' in resource_type:
+            resource_url = element['href']
+            #temp_inline_css += '' + element['href'].text
+
             if resource_url.startswith('//'):
                 # do nothing, complete url
                 resource_url = parsed_url_scheme + ':' + resource_url
@@ -150,59 +118,59 @@ def get_errors_for_link_tags(html, css_features, url, _):
                 # relative url, but without starting /
                 resource_url = parsed_url + '/' + resource_url
 
-            #print('resource_url', resource_url)
+            # print('resource_url', resource_url)
             # print('stylesheet resource #{0}:'.format(resource_index))
-            review_header = '* <link rel="stylesheet" #{0}>:\n'.format(
-                resource_index)
+            # review_header = '* <link rel="stylesheet" #{0}>:\n'.format(
+            #    resource_index)
             # 3.1 GET ERRORS FROM SERVICE (FOR EVERY <LINK>) AND CALCULATE SCORE
-            result_link_css = calculate_rating_for_resource(
-                resource_url, css_features, _, review_header)
-            results.append(result_link_css)
+            results += get_errors_for_url(
+                resource_url)
+            # results.append(result_link_css)
             resource_index += 1
             time.sleep(10)
 
     return results
 
 
-def get_errors_for_style_attributes(html, css_features, _):
+def get_errors_for_style_attributes(html, _):
+    #print('style attribute(s)')
+
+    soup = BeautifulSoup(html, 'lxml')
+    elements = soup.find_all(attrs={"style": True})
+
     results = list()
-    regex = r"<(?P<tag>[a-z0-1]+) .*style=[\"|'](?P<css>[^\"|']+)"
-    matches = re.finditer(regex, html, re.MULTILINE)
     temp_attribute_css = ''
-    for matchNum, match in enumerate(matches, start=1):
-        attribute_tag = match.group('tag')
-        attribute_style = match.group('css')
-        # limit number of calls to service (combine rules)
-        temp_attribute_css += "{0}{{{1}}}".format(
-            attribute_tag, attribute_style)
+
+    for element in elements:
+        # print(element.contents)
+        temp_attribute_css += '' + "{0}{{{1}}}".format(
+            element.name, element['style'])
 
     if temp_attribute_css != '':
-        # print('style-attribute(s):')
-        review_header = '* style="...":\n'
-        result_attribute_css = calculate_rating_for_markup(
-            temp_attribute_css, css_features, _, review_header)
-        results.append(result_attribute_css)
+        results = get_errors_for_css(temp_attribute_css)
         temp_attribute_css = ''
         time.sleep(10)
 
     return results
 
 
-def get_errors_for_style_tags(html, css_features, _):
-    regex = r"<style.*>(?P<css>[^<]+)<\/style>"
-    matches = re.finditer(regex, html, re.MULTILINE)
+def get_errors_for_style_tags(html, _):
+    #print('style tag(s)')
+
+    soup = BeautifulSoup(html, 'lxml')
+    elements = soup.find_all('style')
+
     results = list()
     temp_inline_css = ''
-    for matchNum, match in enumerate(matches, start=1):
-        inline_style = match.group('css')
-        # limit number of calls to service (combine rules)
-        temp_inline_css += inline_style
+    for element in elements:
+        # print(element.contents)
+        temp_inline_css += '' + element.text
+
     if temp_inline_css != '':
         # print('style-tag(s):')
-        review_header = '* <style>:\n'
-        result_inline_css = calculate_rating_for_markup(
-            temp_inline_css, css_features, _, review_header)
-        results.append(result_inline_css)
+        #review_header = '* <style>:\n'
+        results = get_errors_for_css(temp_inline_css)
+        # results.append(result_inline_css)
         temp_inline_css = ''
         time.sleep(10)
     return results
@@ -249,6 +217,8 @@ def get_errors_for_css(data):
     try:
         data = data.strip()
 
+        # print('data:', data)
+
         service_url = 'https://validator.w3.org/nu/'
         headers = {'user-agent': useragent,
                    'Content-Type': 'text/css; charset=utf-8'}
@@ -265,6 +235,8 @@ def get_errors_for_css(data):
         response = json.loads(request.text)
         errors = response['messages']
 
+        #print('errors css:', errors)
+
         return errors
         # print(len(errors))
     except requests.Timeout:
@@ -272,20 +244,8 @@ def get_errors_for_css(data):
         return None
 
 
-def calculate_rating_for_markup(data, css_features, _, review_header):
-    errors = get_errors_for_css(data)
-    result = create_review_and_rating(errors, css_features, _, review_header)
-    return result
-
-
-def calculate_rating_for_resource(url, css_features, _, review_header):
-    errors = get_errors_for_url(url)
-    result = create_review_and_rating(errors, css_features, _, review_header)
-    return result
-
-
 def get_mdn_web_docs_css_features():
-    css_features = list()
+    css_features = {}
 
     html = httpRequestGetContent(
         'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference')
@@ -297,33 +257,42 @@ def get_mdn_web_docs_css_features():
         if index_element:
             links = index_element.find_all('a')
             for link in links:
-                #print('link: {0}'.format(link.string))
+                # print('link: {0}'.format(link.string))
                 regex = '(?P<name>[a-z\-0-9]+)[ ]*'
                 matches = re.search(regex, link.string)
                 if matches:
                     property_name = matches.group('name')
                     # print('-', property_name)
-                    css_features.append(property_name)
+                    # css_features.append(property_name)
+                    css_features["{0}".format(
+                        property_name)] = link.get('href')
         else:
             print('no index element found')
     except:
         print(
             'Error! "{0}" '.format(sys.exc_info()[0]))
         pass
-
-    result = list()
-    for item in css_features:
-        result.append('Property “{0}” doesn\'t exist'.format(item))
-
-    return result
+    return css_features
 
 
 css_features = get_mdn_web_docs_css_features()
 
 
-def create_review_and_rating(errors, css_features, _, review_header):
+def get_properties_doesnt_exist_list():
+    result = list()
+    css_features_keys = css_features.keys()
+    for item in css_features_keys:
+        result.append('Property “{0}” doesn\'t exist'.format(item))
+
+    return result
+
+
+css_properties_doesnt_exist = get_properties_doesnt_exist_list()
+
+
+def create_review_and_rating(errors, _, review_header):
     review = ''
-    whitelisted_words = css_features
+    whitelisted_words = css_properties_doesnt_exist
 
     whitelisted_words.append('“100%” is not a “font-stretch” value')
 
@@ -345,7 +314,7 @@ def create_review_and_rating(errors, css_features, _, review_header):
             if not is_whitelisted:
                 error_message_dict[error_message] = "1"
 
-                #print(' - error-message:', error_message)
+                # print(' - error-message:', error_message)
 
                 if css_review_group_errors:
                     error_message = re.sub(
@@ -369,7 +338,7 @@ def create_review_and_rating(errors, css_features, _, review_header):
 
                 review += _('TEXT_REVIEW_ERRORS_ITEM').format(item_text, item_value)
 
-    #print(' - number_of_errors:', number_of_errors)
+    # print(' - number_of_errors:', number_of_errors)
 
     number_of_error_types = len(error_message_grouped_dict)
 

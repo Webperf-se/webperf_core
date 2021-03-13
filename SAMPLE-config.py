b8946a8635e21f711a55d5058fba07662358d8c7
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2021-03-13 23:26:01 +0100

    re added css test to webperf-core

--- SAMPLE-config.py
+++ SAMPLE-config.py
@@ -1,9 +1,8 @@
-#-*- coding: utf-8 -*-
 """
 Configurations for the client lib
 """
 
-#### NOTE: Rename this file to 'config.py' and fill in the missing info below
+# NOTE: Rename this file to 'config.py' and fill in the missing info below
 
 # useragent for HTTP requests
 useragent = 'Mozilla/5.0 (compatible; Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56'
@@ -15,3 +14,6 @@ http_request_timeout = 60
 
 # timeout between calls to website
 webbkoll_sleep = 20
+
+# if you want a more detailed review for the CSS test (Test #7), set this to False
+css_review_group_errors = True

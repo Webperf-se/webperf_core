8ef7fccb08f1c3d782f3b9d81a1ea9cec726ea7b
Mattias <github-4c85@minadress.nu>
2021-06-20 21:43:14 +0200

    new rating for efficiency test

--- tests/energy_efficiency_websitecarbon.py
+++ tests/energy_efficiency_websitecarbon.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from models import Rating
+import datetime
 import sys
 import socket
 import ssl
@@ -14,7 +15,7 @@ from decimal import Decimal
 import config
 from tests.utils import *
 import gettext
-_ = gettext.gettext
+_local = gettext.gettext
 
 
 def run_test(_, langCode, url):
@@ -27,9 +28,12 @@ def run_test(_, langCode, url):
     language = gettext.translation(
         'energy_efficiency_websitecarbon', localedir='locales', languages=[langCode])
     language.install()
-    _ = language.gettext
+    _local = language.gettext
 
-    print(_("TEXT_RUNNING_TEST"))
+    print(_local("TEXT_RUNNING_TEST"))
+
+    print(_('TEXT_TEST_START').format(
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
     result_json = httpRequestGetContent(
         'https://api.websitecarbon.com/site?url={0}'.format(url))
@@ -56,24 +60,27 @@ def run_test(_, langCode, url):
     # print(points)
 
     if points <= 5:
-        review = _("TEXT_WEBSITE_IS_VERY_GOOD")
+        review = _local("TEXT_WEBSITE_IS_VERY_GOOD")
     elif points >= 4:
-        review = _("TEXT_WEBSITE_IS_GOOD")
+        review = _local("TEXT_WEBSITE_IS_GOOD")
     elif points >= 3:
-        review = _("TEXT_WEBSITE_IS_OK")
+        review = _local("TEXT_WEBSITE_IS_OK")
     elif points >= 2:
-        review = _("TEXT_WEBSITE_IS_BAD")
+        review = _local("TEXT_WEBSITE_IS_BAD")
     elif points <= 1:
-        review = _("TEXT_WEBSITE_IS_VERY_BAD")
+        review = _local("TEXT_WEBSITE_IS_VERY_BAD")
 
-    review += _("TEXT_GRAMS_OF_CO2").format(round(co2, 2))
-    review += _("TEXT_BETTER_THAN").format(cleaner_than)
+    review += _local("TEXT_GRAMS_OF_CO2").format(round(co2, 2))
+    review += _local("TEXT_BETTER_THAN").format(cleaner_than)
     if 'false' in green.lower():
-        review += _("TEXT_GREEN_ENERGY_FALSE")
+        review += _local("TEXT_GREEN_ENERGY_FALSE")
     elif 'true' in green.lower():
-        review += _("TEXT_GREEN_ENERGY_TRUE")
+        review += _local("TEXT_GREEN_ENERGY_TRUE")
 
-    rating = Rating()
+    rating = Rating(_)
     rating.set_overall(points, review)
+
+    print(_('TEXT_TEST_END').format(
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
     return (rating, result_dict)

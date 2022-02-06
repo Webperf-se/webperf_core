375e8b41df89c6c77b99495b995bb34373eb8bb3
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2022-02-06 18:35:36 +0100

    revalidating top 20 websites with our new test

--- tests/tracking_validator_pagexray.py
+++ tests/tracking_validator_pagexray.py
@@ -678,8 +678,8 @@ def get_rating_from_sitespeed(url, _local, _):
     rating = Rating(_, review_show_improvements_only)
 
     # TODO: CHANGE THIS IF YOU WANT TO DEBUG
-    #result_folder_name = 'results-{0}'.format(str(uuid.uuid4()))
-    result_folder_name = 'results'
+    result_folder_name = 'results-{0}'.format(str(uuid.uuid4()))
+    #result_folder_name = 'results'
 
     from tests.performance_sitespeed_io import get_result as sitespeed_run_test
     sitespeed_arg = '--rm --shm-size=1g -b chrome --plugins.remove screenshot --browsertime.chrome.collectPerfLog --browsertime.chrome.includeResponseBodies "all" --html.fetchHARFiles true --outputFolder {2} --firstParty --utc true --xvfb --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(

2c9af2ce4a1128eda42fee19de1930558880c450
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2022-12-04 13:27:46 +0100

    still on disallow list?

--- tests/email_validator.py
+++ tests/email_validator.py
@@ -192,7 +192,7 @@ def run_test(_, langCode, url):
         try:
             # print('SMTP CONNECT:', ip_address)
             with smtplib.SMTP(ip_address, port=25, timeout=request_timeout) as smtp:
-                smtp.ehlo()
+                smtp.ehlo_or_helo_if_needed()
                 smtp.starttls()
                 smtp.noop()
             ipv4_servers_operational_starttls.append(ip_address)
@@ -207,7 +207,7 @@ def run_test(_, langCode, url):
         try:
             # print('SMTP CONNECT:', ip_address)
             with smtplib.SMTP(ip_address, port=25, timeout=request_timeout) as smtp:
-                smtp.ehlo()
+                smtp.ehlo_or_helo_if_needed()
                 smtp.starttls()
                 smtp.noop()
             ipv6_servers_operational_starttls.append(ip_address)

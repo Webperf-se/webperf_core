152c8df18a95f47f0e373e1c1ab6a3061092c002
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2022-12-17 17:26:52 +0100

    os specific code are that great BUT...

--- tests/tracking_validator.py
+++ tests/tracking_validator.py
@@ -693,10 +693,11 @@ def get_rating_from_sitespeed(url, _local, _):
 
     from tests.performance_sitespeed_io import get_result as sitespeed_run_test
 
-    # sitespeed_arg = '--shm-size=1g -b chrome --plugins.remove screenshot --browsertime.chrome.collectPerfLog --browsertime.chrome.includeResponseBodies "all" --html.fetchHARFiles true --outputFolder {2} --firstParty --utc true --xvfb --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(
-    #     config.sitespeed_iterations, url, result_folder_name)
-    sitespeed_arg = '--shm-size=1g -b chrome --plugins.remove screenshot --browsertime.chrome.collectPerfLog --browsertime.chrome.includeResponseBodies "all" --html.fetchHARFiles true --outputFolder {2} --firstParty --utc true --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(
+    sitespeed_arg = '--shm-size=1g -b chrome --plugins.remove screenshot --browsertime.chrome.collectPerfLog --browsertime.chrome.includeResponseBodies "all" --html.fetchHARFiles true --outputFolder {2} --firstParty --utc true --xvfb --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(
         config.sitespeed_iterations, url, result_folder_name)
+    if 'nt' in os.name:
+        sitespeed_arg = '--shm-size=1g -b chrome --plugins.remove screenshot --browsertime.chrome.collectPerfLog --browsertime.chrome.includeResponseBodies "all" --html.fetchHARFiles true --outputFolder {2} --firstParty --utc true --browsertime.chrome.args ignore-certificate-errors -n {0} {1}'.format(
+            config.sitespeed_iterations, url, result_folder_name)
 
     sitespeed_run_test(sitespeed_use_docker, sitespeed_arg)
 

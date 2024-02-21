fb7e4749b40a8f46f93f920756f37d4b01646b7c
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2024-02-21 19:16:52 +0100

    add cache support for dns lookups

--- tests/utils.py
+++ tests/utils.py
@@ -300,6 +300,13 @@ def is_sitemap(content):
 def dns_lookup(hostname, record_type):
     names = list()
 
+    cache_key = 'dnslookup://{0}#{1}'.format(hostname, record_type)
+    content = get_cache_file(
+        cache_key, True, cache_time_delta)
+    if content != None:
+        names = json.loads(content)
+        return names
+
     try:
         dns_records = dns.resolver.resolve(hostname, record_type)
     except dns.resolver.NXDOMAIN:
@@ -325,6 +332,10 @@ def dns_lookup(hostname, record_type):
 
         # sleep so we don't get banned for to many queries on DNS servers
     time.sleep(1)
+
+    nice_raw = json.dumps(names, indent=2)
+    set_cache_file(cache_key, nice_raw, True)
+
     return names
 
 

ee733546bb571fb957906b2efd8c02a44e23586c
Johan Kronberg <johan.kronberg@krompaco.nu>
2024-04-13 15:04:29 +0200

    Add documention for using Docker

--- _run.ps1
+++ _run.ps1
@@ -1,3 +1,3 @@
 Write-Host "When docker has run an you are at bash inside container, you can do for example:"
-Write-Host "python default.py -u https://webperf.se -t 26 -o reports/webperf-se-t-26.json -r"
+Write-Host "python default.py -u https://webperf.se -t 26 -r"
 docker run -it --cap-add=SYS_ADMIN --cpus=".9" --shm-size=3g --rm webperf-runner:0.0.2 bash

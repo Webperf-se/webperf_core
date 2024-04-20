1310429c12e10a4b51fbfa1c95b6dca3db4f2cd7
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2024-04-20 16:40:23 +0200

    pylint docstrings

--- tests/sitespeed_base.py
+++ tests/sitespeed_base.py
@@ -17,7 +17,15 @@ USE_CACHE = get_config_or_default('CACHE_WHEN_POSSIBLE')
 CACHE_TIME_DELTA = get_config_or_default('CACHE_TIME_DELTA')
 
 def to_firefox_url_format(url):
+    """
+    Converts a given URL to Firefox URL format.
 
+    Args:
+        url (str): The URL to be converted.
+
+    Returns:
+        str: The URL in Firefox format.
+    """
     o = urllib.parse.urlparse(url)
     path = o.path
     if '' == o.path:
@@ -33,6 +41,18 @@ def to_firefox_url_format(url):
     return url2
 
 def get_result(url, sitespeed_use_docker, sitespeed_arg, timeout):
+    """
+    Retrieves the result of a site speed test for a given URL.
+
+    Args:
+        url (str): The URL to be tested.
+        sitespeed_use_docker (bool): Whether to use Docker for the site speed test.
+        sitespeed_arg (str): The arguments for the site speed test.
+        timeout (int): The maximum time to wait for the test to complete.
+
+    Returns:
+        tuple: The name of the result folder and the filename of the HAR file.
+    """
     folder = 'tmp'
     if USE_CACHE:
         folder = 'cache'
@@ -71,6 +91,16 @@ def get_result(url, sitespeed_use_docker, sitespeed_arg, timeout):
     return (result_folder_name, '')
 
 def get_cached_result(url, hostname):
+    """
+    Retrieves the cached result for a given URL and hostname.
+
+    Args:
+        url (str): The URL to be tested.
+        hostname (str): The hostname of the site.
+
+    Returns:
+        tuple: The name of the result folder and the filename of the HAR file.
+    """
     # added for firefox support
     url2 = to_firefox_url_format(url)
 
@@ -93,6 +123,15 @@ def get_cached_result(url, hostname):
     return result_folder_name,filename
 
 def get_versions(test):
+    """
+    Extracts the versions from the test results.
+
+    Args:
+        test (str): The test results as a string.
+
+    Returns:
+        dict: A dictionary containing the versions.
+    """
     regex = r"VERSIONS:START: (?P<VERSIONS>[^V]+) VERSIONS:END"
     versions = '{}'
     matches = re.finditer(
@@ -103,6 +142,15 @@ def get_versions(test):
     return versions_json
 
 def get_cookies(test):
+    """
+    Extracts the cookies from the test results.
+
+    Args:
+        test (str): The test results as a string.
+
+    Returns:
+        dict: A dictionary containing the cookies.
+    """
     regex = r"COOKIES:START: {\"cookies\":(?P<COOKIES>.+)} COOKIES:END"
     cookies = '{}'
     matches = re.finditer(
@@ -114,12 +162,37 @@ def get_cookies(test):
 
 
 def cleanup_results_dir(browsertime_path, path):
+    """
+    Cleans up the results directory by renaming the browsertime file and
+    removing the directory.
+
+    Args:
+        browsertime_path (str): The path to the browsertime file.
+        path (str): The path to the directory to be removed.
+    """
     correct_path = f'{path}.har'
     os.rename(browsertime_path, correct_path)
     shutil.rmtree(path)
 
 def get_result_using_no_cache(sitespeed_use_docker, arg, timeout):
+    """
+    Executes a command using subprocess.Popen and returns the result.
 
+    If `sitespeed_use_docker` is True, the command is run in a Docker container.
+    Otherwise, it is run on the host system. The command is constructed using
+    the provided `arg` and `timeout` parameters.
+
+    If the command execution exceeds the `timeout` multiplied by 10, a 
+    subprocess.TimeoutExpired exception is raised, and the process is terminated.
+
+    Parameters:
+    sitespeed_use_docker (bool): Flag to determine if command should be run in Docker.
+    arg (str): Argument to be passed to the command.
+    timeout (int): Time limit for the command execution.
+
+    Returns:
+    str: Output of the command execution.
+    """
     result = ''
     process = None
     process_failsafe_timeout = timeout * 10
@@ -169,6 +242,24 @@ def get_result_using_no_cache(sitespeed_use_docker, arg, timeout):
     return result
 
 def get_sanitized_browsertime(input_filename):
+    """
+    Reads a file and returns its content after removing any character
+    that doesn't match a specific regex pattern.
+
+    The function opens the file with the given `input_filename`,
+    reads its lines, and appends them to a list. 
+    It then joins the lines into a single string and
+    applies a regex substitution to remove any character 
+    that doesn't match the specified pattern.
+    If the file cannot be opened, an error message is printed and 
+    an empty string is returned.
+
+    Parameters:
+    input_filename (str): The name of the file to be read and sanitized.
+
+    Returns:
+    str: The sanitized content of the file.
+    """
     lines = []
     try:
         with open(input_filename, 'r', encoding='utf-8') as file:
@@ -188,6 +279,24 @@ def get_sanitized_browsertime(input_filename):
 
 
 def modify_browsertime_content(input_filename, cookies, versions):
+    """
+    Modifies the content of a browsertime JSON file by adding cookies and software versions, 
+    and removing unnecessary information.
+
+    The function reads the file, sanitizes its content, and loads it as a JSON object. 
+    It then adds cookies and software versions to the 'log' section of the JSON object. 
+    Unnecessary information such as 'version', 'browser', 'creator', and some parts of 'pages'
+    and 'entries' are removed. If any modification is made,
+    the JSON object is written back to the file.
+
+    Parameters:
+    input_filename (str): The name of the file to be modified.
+    cookies (dict): The cookies to be added to the 'log' section.
+    versions (dict): The software versions to be added to the 'log' section.
+
+    Returns:
+    str: The sanitized content of the file before modification.
+    """
     result = get_sanitized_browsertime(input_filename)
     json_result = json.loads(result)
     has_minified = False
@@ -229,6 +338,23 @@ def modify_browsertime_content(input_filename, cookies, versions):
     return result
 
 def modify_browertime_content_entity(entry):
+    """
+    Modifies a browsertime entry by removing unnecessary keys.
+
+    The function iterates over the keys of the entry and its 'request'
+    and 'response' sub-entries. 
+    It removes any key that is not necessary,
+    leaving only 'request', 'response', 'serverIPAddress', 
+    'httpVersion' in the main entry, 'url' in the
+    'request' sub-entry, and 'content', 'headers', 
+    'httpVersion' in the 'response' sub-entry.
+
+    Parameters:
+    entry (dict): The browsertime entry to be modified.
+
+    Returns:
+    bool: True if any modification is made, False otherwise.
+    """
     keys_to_remove = []
     for key in entry.keys():
         if key not in ('request', 'response', 'serverIPAddress','httpVersion'):
@@ -256,10 +382,29 @@ def modify_browertime_content_entity(entry):
 
 
 def write_json(filename, data):
+    """
+    Writes a Python object to a JSON file.
+
+    The function opens the file with the given `filename` in write mode and 
+    uses the json.dump() function to write the `data` to the file.
+
+    Parameters:
+    filename (str): The name of the file to be written.
+    data (dict): The Python object to be written to the file.
+    """
     with open(filename, 'w', encoding='utf-8') as outfile:
         json.dump(data, outfile)
 
 def get_browsertime_har_path(parent_path):
+    """
+    Recursively searches for a file named 'browsertime.har' in a directory structure.
+
+    Args:
+        parent_path (str): The path of the directory to start the search from.
+
+    Returns:
+        str: The path of the 'browsertime.har' file if found, else None.
+    """
     if not os.path.exists(parent_path):
         return None
 

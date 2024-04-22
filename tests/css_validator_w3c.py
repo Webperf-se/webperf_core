cd1b1238b542041c3f3741f310756d5c5dbe1a64
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2024-04-22 17:08:45 +0200

    pylint docstrings

--- tests/css_validator_w3c.py
+++ tests/css_validator_w3c.py
@@ -90,6 +90,20 @@ def run_test(global_translation, lang_code, url):
     return (rating, result_dict)
 
 def handle_entry(entry, global_translation, url, local_translation, result_dict):
+    """
+    Handles an entry in the webpage data, checks for CSS related errors and rates them.
+
+    Parameters:
+    entry (dict): The entry in the webpage data to handle.
+    global_translation (function): Function to translate text globally.
+    url (str): The URL of the webpage.
+    local_translation (function): Function to translate text locally.
+    result_dict (dict): Dictionary containing results of previous checks.
+
+    Returns:
+    list: All link resources found in the entry.
+    Rating: The rating after evaluating the CSS related errors in the entry.
+    """
     all_link_resources = []
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     req_url = entry['url']
@@ -135,6 +149,18 @@ def handle_entry(entry, global_translation, url, local_translation, result_dict)
     return all_link_resources, rating
 
 def rate_css(global_translation, local_translation, data, result_dict):
+    """
+    Rates the CSS of a webpage based on various criteria.
+
+    Parameters:
+    global_translation (function): Function to translate text globally.
+    local_translation (function): Function to translate text locally.
+    data (dict): Data about the webpage resources.
+    result_dict (dict): Dictionary containing results of previous checks.
+
+    Returns:
+    Rating: The final rating after evaluating the CSS of the webpage.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     has_css_contenttypes = False
     errors = []
@@ -182,6 +208,16 @@ def rate_css(global_translation, local_translation, data, result_dict):
     return rating
 
 def rate_css_contenttypes(global_translation, local_translation):
+    """
+    This function rates the content types of CSS files based on certain criteria.
+
+    Parameters:
+    global_translation (function): A function to translate text globally.
+    local_translation (function): A function to translate text locally.
+
+    Returns:
+    Rating: The final rating after evaluating the content types of the CSS files.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating.set_overall(5.0)
@@ -199,6 +235,16 @@ def rate_css_contenttypes(global_translation, local_translation):
     return rating
 
 def rate_css_files(global_translation, local_translation):
+    """
+    This function rates CSS files based on certain criteria.
+
+    Parameters:
+    global_translation (function): A function to translate text globally.
+    local_translation (function): A function to translate text locally.
+
+    Returns:
+    Rating: The final rating after evaluating the CSS files.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating.set_overall(5.0)
@@ -216,6 +262,20 @@ def rate_css_files(global_translation, local_translation):
     return rating
 
 def rate_style_attributes(global_translation, local_translation):
+    """
+    Rates the style attributes of a webpage based on certain standards. 
+
+    The function creates two Rating objects, one for error types and one for errors. 
+    Both ratings are initialized with a perfect score of 5.0. The function then adds 
+    these ratings to a cumulative rating and returns it.
+
+    Parameters:
+    global_translation (function): Function to translate text to a global language.
+    local_translation (function): Function to translate text to a local language.
+
+    Returns:
+    Rating: A cumulative rating of the style attributes.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating.set_overall(5.0)
@@ -232,6 +292,20 @@ def rate_style_attributes(global_translation, local_translation):
     return rating
 
 def rate_style_elements(global_translation, local_translation):
+    """
+    Rates the style elements of a webpage based on certain standards. 
+
+    The function creates two Rating objects, one for error types and one for errors. 
+    Both ratings are initialized with a perfect score of 5.0. The function then adds 
+    these ratings to a cumulative rating and returns it.
+
+    Parameters:
+    global_translation (function): Function to translate text to a global language.
+    local_translation (function): Function to translate text to a local language.
+
+    Returns:
+    Rating: A cumulative rating of the style elements.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating.set_overall(5.0)
@@ -248,6 +322,31 @@ def rate_style_elements(global_translation, local_translation):
     return rating
 
 def get_result(url):
+    """
+    This function performs a performance analysis of a given URL using the Sitespeed.io tool.
+
+    The function constructs a string of command-line arguments for the Sitespeed.io tool,
+    specifying various settings such as the browser to use (Chrome), the plugins to remove,
+    the screenshot settings, the visual metrics settings, 
+    the headless mode, the response bodies to include, the time zone, and the number of iterations.
+
+    If the operating system is not Windows, the function also enables the Xvfb setting.
+    The function then appends two postScript arguments to the command-line arguments string.
+
+    The function calls the get_sitespeed_result function to run the Sitespeed.io tool with
+    the constructed command-line arguments and get the result.
+    The result is a filename of a file that contains the performance analysis result.
+
+    Finally,
+    the function calls the identify_files function to process the performance analysis result and
+    returns the processed data.
+
+    Parameters:
+    url (str): The URL of the web page to analyze.
+
+    Returns:
+    dict: A dictionary that contains the processed performance analysis result.
+    """
     sitespeed_iterations = 1
     sitespeed_arg = (
         '--shm-size=1g '
@@ -282,6 +381,32 @@ def get_result(url):
     return data
 
 def get_errors_for_link_tags(html, url):
+    """
+    This function extracts all link elements from the given HTML content,
+    checks if they are CSS links, resolves their URLs,
+    and checks for errors in the CSS rules associated with these URLs.
+
+    The function uses BeautifulSoup to parse the HTML content and find all link elements.
+    It then checks each link element to see if it is a CSS link.
+    A link element is considered a CSS link if its 'rel' attribute contains 'stylesheet' or
+    if its 'rel' attribute contains 'prefetch' and its 'as' attribute is 'style'.
+
+    If a link element is a CSS link and has an 'href' attribute,
+    the function resolves the URL of the CSS resource associated with the link element.
+    The function supports absolute URLs, protocol-relative URLs, root-relative URLs, 
+    and path-relative URLs.
+
+    Finally, the function checks for errors in the CSS rules associated with each resolved URL,
+    and returns a tuple containing the list of resolved URLs and the list of errors found.
+
+    Parameters:
+    html (str): The HTML content of the web page.
+    url (str): The URL of the web page.
+
+    Returns:
+    tuple: A tuple where the first element is a list of resolved URLs of CSS resources, 
+           and the second element is a list of errors found in the CSS rules.
+    """
     results = []
 
     soup = BeautifulSoup(html, 'lxml')
@@ -318,10 +443,6 @@ def get_errors_for_link_tags(html, url):
                 # relative url, but without starting /
                 resource_url = parsed_url + '/' + resource_url
 
-            # print('resource_url', resource_url)
-            # print('stylesheet resource #{0}:'.format(resource_index))
-            # review_header = '* <link rel="stylesheet" #{0}>:\n'.format(
-            #    resource_index)
             # 3.1 GET ERRORS FROM SERVICE (FOR EVERY <LINK>) AND CALCULATE SCORE
             results += get_errors_for_url(
                 resource_url)
@@ -332,6 +453,31 @@ def get_errors_for_link_tags(html, url):
 
 
 def get_errors_for_style_attributes(url, html):
+    """
+    This function extracts all HTML elements with style attributes from the given HTML content, 
+    constructs CSS rules from these style attributes, and checks for errors in these rules.
+
+    The function uses BeautifulSoup to parse the HTML content and
+    find all elements with style attributes. 
+    It then constructs a string of CSS rules from these style attributes,
+    with each rule consisting of the element name and the style attribute content.
+
+    If the constructed CSS string is not empty,
+    the function creates a temporary URL by appending '#styles-attributes' to the given URL,
+    and stores the CSS string in a cache file associated with this temporary URL. 
+
+    Finally, the function checks for errors in the CSS rules associated with the temporary URL,
+    and returns a tuple containing the list of elements with style attributes and
+    the list of errors found.
+
+    Parameters:
+    url (str): The URL of the web page.
+    html (str): The HTML content of the web page.
+
+    Returns:
+    tuple: A tuple where the first element is a list of elements with style attributes, 
+           and the second element is a list of errors found in the CSS rules.
+    """
     soup = BeautifulSoup(html, 'lxml')
     elements = soup.find_all(attrs={"style": True})
 
@@ -351,6 +497,22 @@ def get_errors_for_style_attributes(url, html):
 
 
 def get_errors_for_style_tags(url, html):
+    """
+    Extracts the 'style' tags from the HTML of a given URL and checks for CSS errors.
+
+    The function uses BeautifulSoup to parse the HTML and find all 'style' elements.
+    The text of these elements is concatenated and checked for CSS errors.
+    If any inline CSS is found,
+    it is temporarily saved and the URL is updated to include '#style-elements'.
+    The CSS errors for this updated URL are then retrieved.
+
+    Parameters:
+    url (str): The URL of the webpage to check.
+    html (str): The HTML of the webpage to check.
+
+    Returns:
+    tuple: A tuple containing the 'style' elements and the CSS errors for the URL.
+    """
     soup = BeautifulSoup(html, 'lxml')
     elements = soup.find_all('style')
 
@@ -369,6 +531,24 @@ def get_errors_for_style_tags(url, html):
 
 
 def calculate_rating(number_of_error_types, number_of_errors):
+    """
+    Calculates and returns the ratings based on the number of error types and
+    total number of errors.
+
+    The rating for number of error types is calculated
+    as 5.0 minus the number of error types divided by 5.0.
+    The rating for number of errors is calculated
+    as 5.0 minus half of the number of errors divided by 5.0.
+    Both ratings are ensured to be at least 1.0.
+
+    Parameters:
+    number_of_error_types (int): The number of different types of errors.
+    number_of_errors (int): The total number of errors.
+
+    Returns:
+    tuple: A tuple containing the rating for number of error types and
+    the rating for number of errors.
+    """
 
     rating_number_of_error_types = 5.0 - (number_of_error_types / 5.0)
 
@@ -380,10 +560,17 @@ def calculate_rating(number_of_error_types, number_of_errors):
     return (rating_number_of_error_types, rating_number_of_errors)
 
 def get_errors_for_url(url):
+    """
+    Returns CSS errors for a given URL in JSON format.
+    """
     params = {'doc': url, 'out': 'json', 'level': 'error'}
     return get_errors('css', params)
 
 def get_mdn_web_docs_css_features():
+    """
+    Returns a tuple containing 2 lists, first one of CSS features and
+    second of CSS functions keys formatted as non-existent properties.
+    """
     features = {}
     functions = {}
 
@@ -416,6 +603,9 @@ css_functions = css_spec[1]
 
 
 def get_properties_doesnt_exist_list():
+    """
+    Returns a list of CSS feature keys formatted as non-existent properties.
+    """
     result = []
     css_features_keys = css_features.keys()
     for item in css_features_keys:
@@ -423,6 +613,9 @@ def get_properties_doesnt_exist_list():
     return result
 
 def get_function_is_not_a_value_list():
+    """
+    Returns a list of CSS function keys appended with an opening parenthesis.
+    """
     result = []
     css_functions_keys = css_functions.keys()
     for item in css_functions_keys:
@@ -434,6 +627,18 @@ css_functions_no_support = get_function_is_not_a_value_list()
 
 
 def create_review_and_rating(errors, global_translation, local_translation, review_header):
+    """
+    Creates a review and rating based on the provided errors and translations.
+
+    Parameters:
+    errors (list): The list of errors to be reviewed.
+    global_translation (function): The function to translate the global text.
+    local_translation (function): The function to translate the local text.
+    review_header (str): The header of the review.
+
+    Returns:
+    Rating: The overall rating calculated based on the errors and translations.
+    """
     whitelisted_words = css_properties_doesnt_exist
 
     whitelisted_words.append('“100%” is not a “font-stretch” value')
@@ -484,15 +689,48 @@ def create_review_and_rating(errors, global_translation, local_translation, revi
         get_reviews_from_errors(msg_grouped_dict, local_translation)
 
     return rating
+
 def get_error_review(review_header, number_of_errors, local_translation):
+    """
+    Generates a review string for the errors.
+
+    Parameters:
+    review_header (str): The header of the review.
+    number_of_errors (int): The number of error.
+    local_translation (function): The function to translate the review text.
+
+    Returns:
+    str: The review string for the errors.
+    """
     return review_header + local_translation('TEXT_REVIEW_RATING_ITEMS').format(
             number_of_errors, 0.0)
 
 def get_error_types_review(review_header, number_of_error_types, local_translation):
+    """
+    Generates a review string for the error types.
+
+    Parameters:
+    review_header (str): The header of the review.
+    number_of_error_types (int): The number of error types.
+    local_translation (function): The function to translate the review text.
+
+    Returns:
+    str: The review string for the error types.
+    """
     return review_header + local_translation('TEXT_REVIEW_RATING_GROUPED').format(
         number_of_error_types, 0.0)
 
 def get_reviews_from_errors(msg_grouped_dict, local_translation):
+    """
+    Generates a review string from the grouped error messages.
+
+    Parameters:
+    msg_grouped_dict (dict): The dictionary of grouped error messages.
+    local_translation (function): The function to translate the review text.
+
+    Returns:
+    str: The review string generated from the error messages.
+    """
     review = ''
     if len(msg_grouped_dict) > 0:
         error_message_grouped_sorted = sorted(
@@ -507,6 +745,16 @@ def get_reviews_from_errors(msg_grouped_dict, local_translation):
 
 
 def error_has_whitelisted_wording(error_message, whitelisted_words):
+    """
+    Checks if the error message contains any of the whitelisted words.
+
+    Parameters:
+    error_message (str): The error message to be checked.
+    whitelisted_words (list): The list of whitelisted words.
+
+    Returns:
+    bool: True if the error message contains a whitelisted word, False otherwise.
+    """
     in_whitelisted = False
     for whitelisted_word in whitelisted_words:
         if whitelisted_word in error_message:
@@ -518,8 +766,18 @@ def get_rating(global_translation,
                error_types_review,
                error_review,
                result):
+    """
+    Calculates and returns the overall rating based on the given parameters.
 
+    Parameters:
+    global_translation (function): A function to translate text to a global language.
+    error_types_review (str): The review of error types.
+    error_review (str): The review of errors.
+    result (tuple): The result object containing overall and standards ratings.
 
+    Returns:
+    Rating: The overall rating calculated based on error types and errors.
+    """
     rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating = Rating(global_translation, REVIEW_SHOW_IMPROVEMENTS_ONLY)
     errors_type_rating.set_overall(result[0])

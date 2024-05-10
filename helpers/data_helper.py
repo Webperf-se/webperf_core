6fd86146c92690ed04f15bb78a23473f03b90462
cockroacher <163405488+cockroacher@users.noreply.github.com>
2024-05-10 17:19:09 +0200

    pylint docstrings

--- helpers/data_helper.py
+++ helpers/data_helper.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
 def append_domain_entry(domain, category, value, result):
+    """
+    Appends a value to a specific category within a domain in the given result dictionary.
+
+    This function checks if the domain and category exist in the result dictionary. If not, 
+    it creates them. It then checks if the value already exists within the category of the 
+    domain. If it doesn't, the function appends the value.
+
+    Parameters:
+    domain (str): The domain to which the entry should be added.
+    category (str): The category within the domain where the value should be added.
+    value (str): The value to be added to the category within the domain.
+    result (dict): The dictionary to which the domain, category, and value should be added.
+
+    Returns:
+    None: This function doesn't return anything; it modifies the result dictionary in-place.
+    """
     if domain not in result:
         result[domain] = {}
 
@@ -11,6 +27,23 @@ def append_domain_entry(domain, category, value, result):
         result[domain][category].append(value)
 
 def has_domain_entry(domain, category, value, result):
+    """
+    Checks if a specific value exists in a category within a domain in the given result dictionary.
+
+    This function checks if the domain and category exist in the result dictionary. If not, 
+    it returns False. It then checks if the value exists within the category of the domain. 
+    If it doesn't, the function returns False. If all checks pass, the function returns True.
+
+    Parameters:
+    domain (str): The domain to be checked.
+    category (str): The category within the domain to be checked.
+    value (str): The value to be checked within the category of the domain.
+    result (dict): The dictionary in which the domain, category, and value should be checked.
+
+    Returns:
+    bool: True if the value exists within the category of the domain in the result dictionary,
+          False otherwise.
+    """
     if domain not in result:
         return False
 

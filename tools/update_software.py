d51a69f15f2d86242c5056bcb993539c3bab5367
cockroacher <163405488+cockroacher@users.noreply.github.com>
2024-05-11 23:19:18 +0200

    fix use of get_config_or_default

--- tools/update_software.py
+++ tools/update_software.py
@@ -6,11 +6,76 @@ import sys
 import json
 import re
 import os
-from tests.utils import *
 import packaging.version
 
+CONFIG_WARNINGS = {}
+
+def get_config_or_default(name):
+    """
+    Retrieves the configuration value for a given name from the configuration file.
+    If the name does not exist in the configuration file,
+    it attempts to retrieve it from the defaults.config.py file.
+    
+    Parameters:
+    name (str): The name of the configuration value to retrieve.
+
+    Returns:
+    The configuration value associated with the given name.
+
+    Raises:
+    ValueError: If the name does not exist in both the configuration file and
+    the defaults.config.py file.
+
+    Notes:
+    - If the name exists in the defaults.config.py file but not in the configuration file,
+      a warning message is printed.
+    - If the name does not exist in both files,
+      a fatal error message is printed and a ValueError is raised.
+    """
+    # Try get config from our configuration file
+    value = get_config_from_module(name, 'config')
+    if value is not None:
+        return value
+
+    name = name.upper()
+    value = get_config_from_module(name, 'config')
+    if value is not None:
+        return value
+
+    # do we have fallback value we can use in our defaults/config.py file?
+    value = get_config_from_module(name, 'defaults.config')
+    if value is not None:
+        if name not in CONFIG_WARNINGS:
+            CONFIG_WARNINGS[name] = True
+        return value
+
+    return None
+
+def get_config_from_module(config_name, module_name):
+    """
+    Retrieves the configuration value for a given name from the specified module file.
+    
+    Parameters:
+    config_name (str): The name of the configuration value to retrieve.
+    module_name (str): The name of the module the values should be retrieved from.
+
+    Returns:
+    The configuration value associated with the given config_name and module_name.
+    """
+    # do we have fallback value we can use in our defaults/config.py file?
+    try:
+        from importlib import import_module # pylint: disable=import-outside-toplevel
+        tmp_config = import_module(module_name) # pylint: disable=invalid-name
+        if hasattr(tmp_config, config_name):
+            return getattr(tmp_config, config_name)
+    except ModuleNotFoundError:
+        _ = 1
+
+    return None
+
 try:
-    github_adadvisory_database_path = get_config_or_default('SOFTWARE_GITHUB_ADADVISORY_DATABASE_PATH')
+    github_adadvisory_database_path = get_config_or_default(
+        'SOFTWARE_GITHUB_ADADVISORY_DATABASE_PATH')
 except:
     # If software_github_adadvisory_database_path variable is not set in config.py this will be the default
     github_adadvisory_database_path = None
@@ -23,6 +88,7 @@ def main(argv):
 
     update_licenses()
     update_software_info()
+
 
 def update_software_info():
     collection = get_software_sources('software-sources.json')

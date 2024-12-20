171eef32f3148b89c29e9e5b8be549b9e4fd4bd6
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2024-12-20 16:03:06 +0100

    test

--- helpers/release_helper.py
+++ helpers/release_helper.py
@@ -2,6 +2,7 @@ import json
 import os
 import getopt
 from pathlib import Path
+import subprocess
 import sys
 from datetime import datetime
 import packaging.version
@@ -73,29 +74,94 @@ def update_package_lock_json():
             print('packages object is missing')
             return False
 
-        if 'node_modules/sitespeed.io/node_modules/chokidar' not in package_info['packages']:
-            print('chokidar object is missing in packages')
-            return True
+        # Remove all development and optional dependencies
+        print('Looks for npm packages with dev or optional dependencies to remove')
+        for package_key, package in package_info['packages'].items():
+            if 'devDependencies' in package:
+                print('-', package_key, 'was using devDependencies')
+                del package['devDependencies']
+            if 'optionalDependencies' in package:
+                print('-', package_key, 'was using optionalDependencies')
+                del package['optionalDependencies']
 
-        if 'optionalDependencies' not in package_info['packages']['node_modules/sitespeed.io/node_modules/chokidar']:
-            print('optionalDependencies object is missing in node_modules/sitespeed.io/node_modules/chokidar')
-            return True
+        write_package_lock_json(base_directory, package_info)
+        print('')
 
-        package_info['packages']['node_modules/sitespeed.io/node_modules/chokidar']['optionalDependencies'] = {}
+        # Remove unused packages
+        print('Looks for npm packages we don\'t depend on')
+        package_keys_to_remove = [] 
+        for package_key, package in package_info['packages'].items():
+            tmp_key = package_key
+            test = True
+            while test:
+                node_modules_index = tmp_key.find('node_modules/')
 
-        if 'node_modules/sitespeed.io/node_modules/fsevents' not in package_info['packages']:
-            print('node_modules/sitespeed.io/node_modules/fsevents object is missing in packages')
-            return True
+                if node_modules_index != -1:
+                    node_modules_index += len('node_modules/')
+                    tmp_key = tmp_key[node_modules_index:]
 
-        del package_info['packages']['node_modules/sitespeed.io/node_modules/fsevents']
+                test = node_modules_index != -1
+            # print(tmp_key)
+            if tmp_key == '':
+                continue
+            if not is_package_used(tmp_key):
+                print('-', tmp_key)
+                # TODO: remove key from package-lock.json
+                package_keys_to_remove.append(package_key)
 
+    for key_to_remove in package_keys_to_remove:
+        if key_to_remove in package_info['packages']:
+            del package_info['packages'][key_to_remove]
+            write_package_lock_json(base_directory, package_info)
+
+    write_package_lock_json(base_directory, package_info)
+    return True
+
+def write_package_lock_json(base_directory, package_info):
     with open(
                 f'{base_directory}{os.path.sep}package-lock.json',
                 'w',
                 encoding='utf-8'
             ) as json_output_file:
         json.dump(package_info, json_output_file, indent=2)
+
+def is_package_used(package_name):
+    info = get_package_info(package_name)
+    if info == None:
+        return False
+    if 'dependencies' not in info:
+        return False
+
     return True
+
+def get_package_info(package_name):
+    process = None
+    timeout = 5
+    process_failsafe_timeout = timeout * 10
+    try:
+        if 'nt' in os.name:
+            command = (f"npm.cmd ls {package_name} --json")
+        else:
+            command = (f"npm ls {package_name} --json")
+        with subprocess.Popen(
+            command.split(), stdout=subprocess.PIPE) as process:
+            output, error = process.communicate(timeout=process_failsafe_timeout)
+
+            if error is not None:
+                print('DEBUG get_package_info(error)', error)
+
+            json_result = json.loads(output)
+            # nice = json.dumps(json_result, indent=3)
+            # print(package_name, nice)
+            return json_result
+
+    except subprocess.TimeoutExpired:
+        if process is not None:
+            process.terminate()
+            process.kill()
+        print('TIMEOUT!')
+        return None
+    return None
 
 
 def update_release_version(argv):

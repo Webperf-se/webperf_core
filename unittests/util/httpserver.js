ebc388089b88dc31a8c26b9e8ab977b7131768f7
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2025-04-25 21:22:53 +0200

    serve

--- unittests/util/httpserver.js
+++ unittests/util/httpserver.js
@@ -3,14 +3,13 @@ import { createServer } from 'node:http';
 
 let server;
 const port = 3000;
-const host = '127.0.0.1';
 
 export async function startServer() {
   server = createServer((request, response) => {
     return handler(request, response, { public: './unittests/data/' });
   });
 
-  return server.listen(port, host, () => {});
+  return server.listen(port, () => {});
 }
 export async function stopServer() {
   return server.close();

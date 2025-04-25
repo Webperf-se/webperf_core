e52e836f7e9ea2b1ec41ead0b1e2cebc34029f78
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2025-04-25 20:59:40 +0200

    fix for unittests

--- unittests/util/httpserver.js
+++ unittests/util/httpserver.js
@@ -3,13 +3,14 @@ import { createServer } from 'node:http';
 
 let server;
 const port = 3000;
+const host = '127.0.0.1';
 
 export async function startServer() {
   server = createServer((request, response) => {
     return handler(request, response, { public: './unittests/data/' });
   });
 
-  return server.listen(port, () => {});
+  return server.listen(port, host, () => {});
 }
 export async function stopServer() {
   return server.close();
